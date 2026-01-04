d4ba5571e7d801006f40b0ed5fea8ac33e0ecaf4
musharna <96366172+musharna@users.noreply.github.com>
2026-01-04 06:44:09 +0000

    Deploying to gh-pages from @ musharna/musharna.github.io@80be3e6b0cd60373b559a373fa90fcd2bd1f0a2e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "",
+          description: "More projects coming soon!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
